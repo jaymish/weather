8bde03dafd0c412b0c81b983bf94c8c6b27891df
Jaymish <jaymishptel@gmail.com>
2022-02-14 11:44:42 -0800

    initial

---
+++ src/test/java/com/egen/weather1/Weather1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egen.weather1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Weather1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
